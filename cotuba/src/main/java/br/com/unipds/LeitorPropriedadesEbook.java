8da031a712c352d2d5959e070dce1dc7eb62745f
Luiz Alfredo Carrara Bertusso <luizalfredocb@gmail.com>
2026-07-17 19:34:09 -0300

    fix: suporte acentuação

--- cotuba/src/main/java/br/com/unipds/LeitorPropriedadesEbook.java
+++ cotuba/src/main/java/br/com/unipds/LeitorPropriedadesEbook.java
@@ -1,5 +1,6 @@
 package br.com.unipds;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Properties;
 
@@ -11,7 +12,7 @@ public class LeitorPropriedadesEbook {
 		if (arquivoProperties.toFile().exists()) {
 			Properties properties = new Properties();
 
-			try (var inputStream = java.nio.file.Files.newInputStream(arquivoProperties)) {
+			try (var inputStream = java.nio.file.Files.newBufferedReader(arquivoProperties, StandardCharsets.UTF_8)) {
 				properties.load(inputStream);
 
 				ebook.setTitulo(properties.getProperty("cotuba.ebook.titulo"));
